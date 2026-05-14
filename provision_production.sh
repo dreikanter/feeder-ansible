f3cc1dd880343e935ebcca1b893a84062af9beb7
Alex Musayev <alex.musayev@gmail.com>
2026-05-14 21:15:31 +0200

    Add comments to the deployment script

--- provision_production.sh
+++ provision_production.sh
@@ -1,1 +1,5 @@
-ansible-playbook --inventory=inventory/production -vvvv ./provision.yml
+# By default this will release the "release" branch
+# ansible-playbook --inventory=inventory/production -vvvv ./provision.yml
+
+# This command releases explicitly defined branch
+# ansible-playbook --inventory=inventory/production -vvv ./deploy.yml -e git_branch=master
